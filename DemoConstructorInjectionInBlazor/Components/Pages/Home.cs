4249f84663816b00333da31e56480b789f4be0b2
Frederick Asante <Rastinga1995@hotmail.com>
2024-09-23 09:01:44 +0000

    Add project files.

---
+++ DemoConstructorInjectionInBlazor/Components/Pages/Home.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DemoConstructorInjectionInBlazor.Components.Pages
+{
+    public partial class Home
+    {
+        private readonly HttpClient httpClient;
+        private List<Division> Divisions { get; set; } = [];
+        public Home(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+            httpClient.BaseAddress = new Uri("http://localhost:5000/api/");
+            Divisions = httpClient.GetFromJsonAsync<List<Division>>("academic/get-divisions").Result!;
+        }
+
+
+        public List<Division> PublicDivisions = [];
+        protected override void OnInitialized()
+        {
+            PublicDivisions = Divisions.Take(3).ToList();
+        }
+    }
+     
+
+
+    public class Division { public int Id { get; set; } public int Number { get; set; } }
+}
